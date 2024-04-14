b8cb1a69554c19cc49a17192f7b52837fb08d4ce
Rafia <rafiarafi2787@gmail.com>
2024-04-14 15:18:54 -0700

    Q9 commit

---
+++ Q9/main9.ts
@@ -0,0 +1,12 @@
+
+
+
+
+
+// Store your favorite number in a variable. Then, using that variable, create a message that reveals
+//  your favorite number. 
+// Print that message.
+
+let favNumber=10;
+let message="My favourite Number is:";
+console.log(`${message} ${favNumber}`);
