09e36d5553e136864c03eaeaaacf1ca09aa97b8a
Rafia <rafiarafi2787@gmail.com>
2024-04-15 11:10:03 -0700

    Q36 commit

---
+++ Q36/main36.js
@@ -0,0 +1,8 @@
+"use strict";
+//call the function.
+function make_shirt(size, text) {
+    console.log(`creating a ${size} shirt with the message:${text} `);
+}
+make_shirt('small', "I love Madina");
+make_shirt('medium', "SO CUTE");
+make_shirt('large', "Do More TS");
---
+++ Q36/main36.ts
@@ -0,0 +1,12 @@
+//call the function.
+
+function make_shirt (size:string, text:string){
+
+    console.log(`creating a ${size} shirt with the message:${text} `);
+}
+
+    make_shirt ('small',"I love Madina"); 
+
+    make_shirt ('medium',"SO CUTE");
+
+    make_shirt ('large',"Do More TS");
