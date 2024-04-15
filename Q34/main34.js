4c92a45efa66afe111894927d21fcf7897c10a61
Rafia <rafiarafi2787@gmail.com>
2024-04-15 10:57:58 -0700

    Q34 commit

---
+++ Q34/main34.js
@@ -0,0 +1,6 @@
+"use strict";
+let pizzas = ['peproni', 'afghani chiken tikka', 'fajita'];
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+console.log(`I really love pizza!`);
---
+++ Q34/main34.ts
@@ -0,0 +1,8 @@
+let pizzas:string[] = ['peproni','afghani chiken tikka','fajita'];
+
+for(let pizza of pizzas){
+
+
+    console.log(`I like ${pizza} pizza.`);
+}
+console.log(`I really love pizza!`); 
